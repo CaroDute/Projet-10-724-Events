29b6e3ed3d102981221c294d5ca9503089d1884b
CaroDute <caro.duteurtre@orange.fr>
2024-04-29 18:40:17 +0200

    Correction du Slider effectuée

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -4,95 +4,35 @@ import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
-// const Slider = () => {
-//   const { data } = useData();
-//   const [index, setIndex] = useState(0);
-//   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-//     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-//   );
-//   console.log("Data:", data);
-//   const nextCard = () => {
-//     setTimeout(
-//       () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-//       5000
-//     );
-//   };
-//   useEffect(() => {
-//     nextCard();
-//   });
-//   return (
-//     <div className="SlideCardList">
-//       {byDateDesc?.map((event, idx) => (
-//         <>
-//           <div
-//             key={event.title}
-//             className={`SlideCard SlideCard--${
-//               index === idx ? "display" : "hide"
-//             }`}
-//           >
-//             <img src={event.cover} alt="forum" />
-//             <div className="SlideCard__descriptionContainer">
-//               <div className="SlideCard__description">
-//                 <h3>{event.title}</h3>
-//                 <p>{event.description}</p>
-//                 <div>{getMonth(new Date(event.date))}</div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="SlideCard__paginationContainer">
-//             <div className="SlideCard__pagination">
-//               {byDateDesc.map((_, radioIdx) => (
-//                 <input
-//                   key={`${event.id}`}
-//                   type="radio"
-//                   name="radio-button"
-//                   checked={idx === radioIdx}
-//                 />
-//               ))}
-//             </div>
-//           </div>
-//         </>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Slider;
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  );
+
+  const imgSlider = data?.focus?.length // Récupération de la taille du tableau d'origine avant tri. 
+
+  const nextCard = () => {
+    setTimeout(
+      () => setIndex(index < imgSlider -1 ? index + 1 : 0),
+      5000
+    );
+  };
   useEffect(() => {
-    if (data) {
-      const byDateDesc = data?.focus.sort((evtA, evtB) =>
-        new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-      );
-      const nextCard = () => {
-        setTimeout(
-          () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-          5000
-        );
-      };
-      nextCard();
-    }
-  }, [data, index]);
-
-  console.log("data :", data)
-  
-  if (!data) {
-    return null; // Ou un indicateur de chargement si nécessaire
-  }
-
+    nextCard();
+  });
   return (
     <div className="SlideCardList">
-      {data?.focus.map((event, idx) => (
-        <>
+      {byDateDesc?.map((event, idx) => (
+        <div 
+        key={event.title}
+        >
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
-          >
+            >
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -104,17 +44,18 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {data?.focus.map((_, radioIdx) => (
+              {byDateDesc.map((radioInput, radioIdx) => (
                 <input
-                key={`${event.id}`}
+                  key={radioInput.title}
                   type="radio"
                   name="radio-button"
                   checked={idx === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
